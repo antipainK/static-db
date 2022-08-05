76ee073196b54428efb1dbd758f0ca84b46d2cbe
Wojciech Kosztyła <wojtex1221@gmail.com>
2022-08-05 20:59:22 +0200

    Added temporary files for debugging.

---
+++ index.js
@@ -0,0 +1,12 @@
+export {printStupid, returnStupid};
+
+function printStupid(amount) {
+    for (let i in amount) {
+        console.log('stoopid');
+    }
+}
+
+function returnStupid(){
+    return "stoooopid";
+}
+
